3fd98f6449fd59cc6d9ef90ecad375cd62da71e7
Andrew Morton <ahjmorton@gmail.com>
2015-06-15 22:33:12 +0100

    Initial commit with yo microlib start

---
+++ README.md
@@ -0,0 +1,5 @@
+# turtle.js
+
+[![Build Status](https://secure.travis-ci.org/ahjmorton/turtle.js.png?branch=master)](http://travis-ci.org/ahjmorton/turtle.js)
+
+It does this one thing really awesome! boyah!
---
+++ test/spec.js
@@ -0,0 +1,17 @@
+/**
+ * turtle.js
+ *
+ *    Library test
+ */
+
+'use strict'
+
+var assert = require('assert'),
+lib        = require('../lib/turtle.js');
+
+describe('Basic library test', function() {
+  it('should answer all questions with YO!', function() {
+    var answer = lib.turtle.js('Should I tickle this unicorn?');
+    assert.equal(answer,'YO!');
+  })
+})
