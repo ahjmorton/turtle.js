3c81dd6f107fdb1e88f970e2ecc216ab691ce252
Andrew Morton <ahjmorton@gmail.com>
2015-06-27 17:25:35 +0100

    Add basic implementation with basic spec

--- test/spec.js
+++ test/spec.js
@@ -1,17 +1,71 @@
-/**
- * turtle.js
- *
- *    Library test
- */
-
 'use strict'
 
 var assert = require('assert'),
-lib        = require('../lib/turtle.js');
+sinon      = require('sinon'),
+lib        = require('../lib/turtle.js').turtle;
 
-describe('Basic library test', function() {
-  it('should answer all questions with YO!', function() {
-    var answer = lib.turtle('Should I tickle this unicorn?');
-    assert.equal(answer,'YO!');
-  })
-})
+describe('turtle.js', function() {
+
+  var canvasMock, context, subject;
+
+  beforeEach(function() {
+    context = sinon.stub({
+      beginPath : function() { },
+      moveTo : function (oldX, oldY) {},
+      lineTo : function (newX, newY) {},
+      stroke : function() {}
+    });
+    canvasMock = {
+       getContext : function() {
+         return context;
+       }
+    };
+    subject = lib.penFor(canvasMock);
+  });
+
+  
+  it('should not draw when pen is up', function() {
+     subject.penUp().moveTo(200, 200);
+     var movements = [
+       function() { subject.left(100); },
+       function() { subject.right(100); },
+       function() { subject.up(100); },
+       function() { subject.down(100); },
+       function() { subject.moveTo(10, 10);}
+     ];
+     movements.forEach(function(movementFunction) {
+       movementFunction();
+       assert(!context.beginPath.called);
+       assert(!context.moveTo.called);
+       assert(!context.lineTo.called);
+       assert(!context.stroke.called);
+     });
+  });
+
+  it('should draw when pen is down', function() {
+     subject.penDown().moveTo(200, 200);
+     var movements = [
+       function() { subject.left(100); },
+       function() { subject.right(100); },
+       function() { subject.up(100); },
+       function() { subject.down(100); },
+       function() { subject.moveTo(10, 10);}
+     ];
+
+     var functionsToCall = [
+       context.beginPath, 
+       context.moveTo,
+       context.lineTo,
+       context.stroke
+     ];
+     movements.forEach(function(movementFunction) {
+       movementFunction();
+
+       functionsToCall.forEach(function(toCall) {
+         assert(toCall.called);
+         toCall.reset();
+       });
+     });
+  });
+ 
+});
